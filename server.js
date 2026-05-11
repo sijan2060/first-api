c27b9d06497af6304b32a0848ecae47393ebdf2e
Sijan Maharjan <sijanpr07@gmail.com>
2026-05-11 15:58:33 +0545

    Update Message

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require("express");
-const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+console.log(process.env.MONGO_URI);
+const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
+
+
+connectDB();
+const app = express();
 app.use(express.json());
 app.use("/api/users", userRoutes);
+
 app.get("/", (req, res) => {
   res.send("API Running");
 });
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
