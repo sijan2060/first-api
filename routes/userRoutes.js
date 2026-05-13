e17eae1ec6f31f69b80fdfe531bd4953f7a94c34
Sijan Maharjan <sijanpr07@gmail.com>
2026-05-13 20:10:49 +0545

    Update Message

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,17 +1,37 @@
 const express = require("express");
 const router = express.Router();
-router.post("/register", (req, res) => {
-    const { name, email, password} = req.body;
 
-    res.json({
-        message: "User Registered ",
-        user: {
+const User = require("../models/User");
+
+router.post("/register", async (req, res) => {
+
+    try{ 
+        const { name, email, password} = req.body;
+
+        const userExists = await User.findOne({ email})
+        if(userExists){
+            return res.status(400).json({
+                message: "User already exists",
+            });
+        }
+    
+        const user = await User.create({
             name,
             email,
-        },
-    });
+            password,
+        });
 
-});
+        res.status(201).json({
+            message: "User Registered Successfully", 
+            user,
+            });
+        } catch (error) {
+            res.status(500).json({
+                message: error.message,
+            });
+        }
+
+ });
 
 module.exports = router;
 
