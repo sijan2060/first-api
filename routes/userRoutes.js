236eb37658d163abed818d8da9ea7921ae56403c
Sijan Maharjan <sijanpr07@gmail.com>
2026-05-16 20:57:18 +0545

    Update Message

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -37,5 +37,45 @@ router.post("/register", async (req, res) => {
 
  });
 
+ router.post("/login", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        // Find user by email
+        const user = await User.findOne({ email });
+
+        // Check if user exists
+        if (!user) {
+            return res.status(400).json({
+                message: "Invalid email or password",
+            });
+        }
+            // Compare entered password with hashed password 
+            const isMatch = await bcrypt.compare(password, user.password);
+            // Wrong Password
+            if (!isMatch) {
+                return res.status(400).json({
+                    message: "Invalid email or password",
+                });
+            }
+
+            // Login Success 
+            res.status(200).json({
+                message: "Login Successful",
+                user: {
+                    id: user._id,
+                    name: user.name,
+                    email: user.email,
+                },
+            });           
+        } catch (error) {
+            res.status(500).json({
+                message: error.message,
+            });
+        }
+    });
+
+
+
 module.exports = router;
 
